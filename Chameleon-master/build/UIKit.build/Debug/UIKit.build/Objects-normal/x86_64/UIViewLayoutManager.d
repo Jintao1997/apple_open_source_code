b5b980cebdd8dc9ffd031577c1ac320aba10b7a2
jintao <>
2020-04-21 15:39:19 +0800

    add files

---
+++ Chameleon-master/build/UIKit.build/Debug/UIKit.build/Objects-normal/x86_64/UIViewLayoutManager.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/UIKit_Prefix.pch \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/Classes/UIViewLayoutManager.m \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/Classes/UIViewLayoutManager.h \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/Classes/UIView+UIPrivate.h \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/Classes/UIView.h \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/Classes/UIResponder.h \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/Classes/UIEvent.h \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/Classes/UIGeometry.h \
+  /Users/jintao/Documents/GitHub/Git源码/apple_open_source_code/Chameleon-master/UIKit/Classes/UIAppearance.h
